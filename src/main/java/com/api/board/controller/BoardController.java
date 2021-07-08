bb69acbd4b5e589a07c6b476ff6ad870bc4caef2
tychejin1218 <tychejin@naver.com>
2021-07-08 09:16:37 +0900

    [Spring Boot] REST API 게시판 만들기(3) - Logback 설정  - 로그 테스트

--- src/main/java/com/api/board/controller/BoardController.java
+++ src/main/java/com/api/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.api.board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,18 @@ import com.api.board.service.BoardService;
 @RestController
 public class BoardController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
-    private BoardService boardService;
-    
-    @GetMapping
-    public List<Board> getBoardList() throws Exception {
-        return boardService.getBoardList();
-    }
+	private BoardService boardService;
+
+	@GetMapping
+	public List<Board> getBoardList() throws Exception {
+		
+		logger.info("===== ===== ===== =====");
+		logger.info("BoardController.getBoardList()");
+		logger.info("===== ===== ===== =====");
+		
+		return boardService.getBoardList();
+	}
 }
