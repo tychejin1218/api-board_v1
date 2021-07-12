33b2f6df172907e9e1d3710fc2ce4d563b99f74f
tychejin1218 <tychejin@naver.com>
2021-07-12 10:52:23 +0900

    [Spring Boot] REST API 게시판 만들기(5) - Mapper 구현 및 테스트  - BoardMapper.xml 수정  - BoardMapper.java 수정

--- src/main/java/com/api/board/mapper/BoardMapper.java
+++ src/main/java/com/api/board/mapper/BoardMapper.java
@@ -8,4 +8,16 @@ public interface BoardMapper {
 	
 	/** 게시글 목록 조회 */
     public List<Board> getBoardList() throws Exception;
+    
+    /** 게시글 상세 조회 */
+    public Board getBoardDetail(int board_seq) throws Exception;
+    
+    /** 게시글 등록 */
+    public int insertBoard(Board board) throws Exception;  
+    
+    /** 게시글 수정 */
+    public int updateBoard(Board board) throws Exception;
+    
+    /** 게시글 삭제 */
+    public int deleteBoard(int board_seq) throws Exception;
 }
