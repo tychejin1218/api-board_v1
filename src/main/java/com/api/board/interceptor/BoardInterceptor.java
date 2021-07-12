8c2259d89718c62910b6b59ad2fec957d2b5ae96
tychejin1218 <tychejin@naver.com>
2021-07-12 16:33:35 +0900

    [Spring Boot] REST API 게시판 만들기(6) - Interceptor 적용  - BoardInterceptor.java 추가  - WebMvcConfig.java 추가

---
+++ src/main/java/com/api/board/interceptor/BoardInterceptor.java
@@ -0,0 +1,38 @@
+package com.api.board.interceptor;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+ 
+public class BoardInterceptor implements HandlerInterceptor {
+ 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+ 
+    @Override
+    public boolean preHandle( HttpServletRequest request
+    		                , HttpServletResponse response
+    		                , Object handler ) throws Exception {
+        logger.info("===== ===== ===== ===== ===== preHandle");
+        return true;
+    }
+ 
+    @Override
+    public void postHandle( HttpServletRequest request
+    		              , HttpServletResponse response
+    		              , Object handler
+    		              , ModelAndView modelAndView ) throws Exception {
+        logger.info("===== ===== ===== ===== ===== postHandle");
+    }
+ 
+    @Override
+    public void afterCompletion( HttpServletRequest request
+    		                   , HttpServletResponse response
+    		                   , Object handler
+    		                   , Exception ex ) throws Exception {
+        logger.info("===== ===== ===== ===== ===== afterCompletion");
+    }
+}
